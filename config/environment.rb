0ac19059b3d15ee63e03ab6d69185c4aaaae77a4
psnider30 <pbsnider@gmail.com>
2017-07-26 15:50:22 -0700

    Actually add files

---
+++ config/environment.rb
@@ -0,0 +1,19 @@
+require 'rake'
+require 'active_record'
+require 'yaml/store'
+require 'ostruct'
+require 'date'
+
+
+require 'bundler/setup'
+Bundler.require
+
+
+# put the code to connect to the database here
+ActiveRecord::Base.establish_connection(
+:adapter => "sqlite3",
+:database => "db/artists.sqlite"
+)
+
+
+require_relative "../lib/artist.rb"
